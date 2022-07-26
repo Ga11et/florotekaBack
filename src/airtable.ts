98e72f1082cef24476c7c00d519cdfb6391f4859
Fyodor <sivkov1998@yandex.ru>
2022-07-26 17:59:30 +0300

    added authtification

--- src/airtable.ts
+++ src/airtable.ts
@@ -1,11 +1,9 @@
 require('dotenv').config({path: process.cwd() + '/.env'})
-// var Airtable = require('airtable');
-// Airtable.configure({
-//   endpointUrl: 'https://api.airtable.com',
-//   apiKey: process.env.AIRTABLE_API_KEY
-// });
-// var base = Airtable.base(process.env.AIRTABLE_BASE_ID);
-
-const base = require('airtable').base(process.env.AIRTABLE_BASE_ID);
+var Airtable = require('airtable');
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_API_KEY
+});
+var base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 export default base
